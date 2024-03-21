3080f8b4425a9c03d16e9c3222fa09ab1bc724b3
Vieirinhagui <vieiragui147@gmail.com>
2024-03-21 13:35:47 -0300

    NumerosPrimos

---
+++ atividade1/numerosPrismos.js
@@ -0,0 +1,30 @@
+function verificarNumeroPrimo(n) {
+  if (n <= 1) {
+    return false;
+  }
+  if (n <= 3) {
+    return true;
+  }
+  if (n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+  let i = 5;
+  while (i * i <= n) {
+    if (n % i === 0 || n % (i + 2) === 0) {
+      return false;
+    }
+    i += 6;
+  }
+  return true;
+}
+
+console.log(verificarNumeroPrimo(0)); // false
+console.log(verificarNumeroPrimo(1)); // false
+console.log(verificarNumeroPrimo(2)); // true
+console.log(verificarNumeroPrimo(3)); // true
+console.log(verificarNumeroPrimo(7)); // true
+console.log(verificarNumeroPrimo(83)); // true
+console.log(verificarNumeroPrimo(100)); // false
+console.log(verificarNumeroPrimo(991)); // true
+console.log(verificarNumeroPrimo(104729)); // true
+console.log(verificarNumeroPrimo(14348907)); // false
